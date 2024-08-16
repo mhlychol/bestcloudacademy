067b0225de07e170939317e8bd4893c9a52db2d2
Melih ARIKAN <meliharikann@gmail.com>
2024-08-16 04:45:23 +0300

    yorum satırları eklendi, port eklendi

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-# app.py
 from flask import Flask, request, jsonify
 
+# Flask uygulaması oluşturuluyor
 app = Flask(__name__)
 
+# Ana endpoint: "/" GET isteği aldığında "BC4M" mesajını döner
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"msg": "BC4M"})
-
+    
+# Sağlık durumu endpointi: "/health" GET isteği aldığında uygulamanın sağlık durumunu döner
 @app.route('/health', methods=['GET'])
 def health():
     return jsonify({"status": "OK"}), 200
-
+    
+# POST isteği endpointi: "/" POST isteği ile gelen veriyi aynen geri döner
 @app.route('/', methods=['POST'])
 def echo_post():
     data = request.get_json()
     return jsonify(data), 200
-
+    
+# Uygulama başlatma: Uygulama 0.0.0.0 adresinde ve 5000 portunda çalıştırılır
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
